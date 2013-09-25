b5bf0a9177c8823dadab4dca2e097d1ddd04cc11
Apprentice <apprentice@dbc30.local>
2013-09-24 18:13:28 -0700

    able to load sample_boards cards onto page

--- post-it.js
+++ post-it.js
@@ -1,70 +1,64 @@
 var Board = function( selector ) {
-  // Your board related code goes here
-  // Use $elem to access the DOM element for this board
   var $elem = $( selector );
   
   function initialize() {
-    // What needs to happen when this object is created?
-    // Need to listen to the selector element for our board for clicks
-    // Need to append to that element a post it on click
 
     $elem.on('click', function(e){
       var left = e.offsetX
       var top = e.offsetY
-      makeNote($elem, top, left);
+      makeNote($elem, { top: top , left: left });
       makeNoteDraggable();
       makeNoteEditable();
       makeNoteClosable();
     })
   };
 
-  function makeNote(elem, top, left){
-    var newPostIt = new PostIt();
-    elem.append(newPostIt.object);
-    var allNotes = document.getElementsByClassName('post-it')
-    var newDomElement = allNotes[allNotes.length-1];
-    $( newDomElement ).css({
-      "top" : top, 
-      "left": left
-    });
-  };
-
-  function makeNoteDraggable(){
-    $('.post-it').draggable({handle: ".header"});
-  };
-
-  function makeNoteEditable(){
-    $( ".post-it" ).on('click', function( event ) {
-      event.stopPropagation();
-    })
-
-    $( ".post-it .content" ).on('click', function() {
-      $(this).attr("contenteditable", "true")
-    })
-
-  };
-
-  function makeNoteClosable(){
-    $('.exit').on('click', function( event ){
-      event.stopPropagation();
-      $(this).closest('.post-it').remove();
-    })
-  };
-
   initialize();
 };
 
+function makeNote(elem, position, content){
+  var newPostIt = new PostIt(position, content);
+  var $elem = $( elem );
+  $elem.append(newPostIt.object);
+  var allNotes = document.getElementsByClassName('post-it')
+  var newDomElement = allNotes[allNotes.length-1];
+  $( newDomElement ).css({
+    "top" : position.top, 
+    "left": position.left
+  });
+};
 
-var PostIt = function() {
-  // Your post-it related code goes here
-  var post_it_stuff = " class=\"post-it\">"
-  var header_stuff = " class=\"header\"><span class=\"exit\">X</span>"
-  var content_stuff = " class=\"content\">"
+function makeNoteDraggable(){
+  $('.post-it').draggable({handle: ".header"});
+};
+
+function makeNoteEditable(){
+  $( ".post-it" ).on('click', function( event ) {
+    event.stopPropagation();
+  })
+
+  $( ".post-it .content" ).on('click', function() {
+    $(this).attr("contenteditable", "true")
+  })
+
+};
+
+function makeNoteClosable(){
+  $('.exit').on('click', function( event ){
+    event.stopPropagation();
+    $(this).closest('.post-it').remove();
+  })
+};
+
+var PostIt = function(position, content) {
+  var post_it_stuff = ' class="post-it" style="left:'+position.left+'; top:'+position.top+'";>' 
+  var header_stuff = ' class="header"><span class="exit">X</span>'
+  var content_stuff = ' class="content">'
 
   this.object = 
   "<div"+post_it_stuff+
     "<div"+header_stuff+"</div>"+
-    "<div"+content_stuff+"</div>"+
+    "<div"+content_stuff+content+"</div>"
   "</div>"
 };
 
@@ -88,7 +82,6 @@ function loadBoards() {
   $('#load_samples').on('click', function(e){
     e.stopPropagation();
     $.each(SampleBoards, function(board_name, cards) { 
-      // for each board_name we want to do something.
       $('#board_list').append("<li>"+board_name+"</li>")
     })
   loadCertainBoard();
@@ -99,19 +92,18 @@ function loadBoards() {
 function loadCertainBoard() {
   $('#board_list li').on('click', function(e){
     var clickedBoardName = this.innerHTML;
-    
     $.each( eval("SampleBoards."+clickedBoardName) , function(index, card) {
-      console.log(index)
-      console.log(card)
+      makeNote('body', card.position, card.content);
     })
   })  
 }
 
 $(function() {
   loadBoards();
+  // debugger;
+  // prepareBody();
+  // makeNewBoard();
 });
-
-
 
 
 
